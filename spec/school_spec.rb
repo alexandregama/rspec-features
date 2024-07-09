66c57cf70802b647b7691095c9feb1ab6a0770d1
Alexandre Gama <alexandre.gama.lima@gmail.com>
2024-07-09 14:42:37 -0300

    Replaces the before hook by let

--- spec/school_spec.rb
+++ spec/school_spec.rb
@@ -11,12 +11,28 @@ RSpec.describe School do
   before do
     @school = School.new('Beverly Hills')
   end
-  
+
+  def new_school
+    School.new('Beverly Hills')
+  end
+
   it 'has a name' do
-    expect(@school.name).to eq('Beverly Hills')
+    expect(new_school.name).to eq('Beverly Hills')
   end
 
   it 'should start off with no students' do
     expect(@school.students).to eq([])
+  end
+end
+
+RSpec.describe 'School with the let method' do
+  let(:school) { School.new('Beverly Hills') }
+
+  it 'has a name' do
+    expect(school.name).to eq('Beverly Hills')
+  end
+
+  it 'has empty list of students' do
+    expect(school.students).to eq([])
   end
 end
